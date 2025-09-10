0e3690769f308a332dfbb7a310dc5edd463799f6
Nelly <nellybulatova12@email.com>
2025-09-10 15:53:27 +0300

    fix getItems in cartThunks

--- src/redux/features/cart/cartThunks.ts
+++ src/redux/features/cart/cartThunks.ts
@@ -1,3 +1,4 @@
+import qs from "qs";
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { axiosInstance } from "../../../lib/axios";
 import axios from "axios";
@@ -7,29 +8,27 @@ export const getItems = createAsyncThunk<
   Product[],
   GetItemsArgs | undefined,
   { rejectValue: string }
->("items/fetch", async ({ category = [], sort, order } = {}, { rejectWithValue }) => {
-  try {
-    const params = new URLSearchParams();
+>(
+  "items/fetch",
+  async ({ category = [], sort, order } = {}, { rejectWithValue }) => {
+    try {
+      const res = await axiosInstance.get<Product[]>("/products", {
+        params: {
+          category,
+          _sort: sort,
+          _order: order,
+        },
+        paramsSerializer: (params) =>
+          qs.stringify(params, { arrayFormat: "repeat" }),
+      });
+      return res.data;
+    } catch (err: unknown) {
+      const message = axios.isAxiosError(err)
+        ? ((err.response?.data as { message?: string } | undefined)?.message ??
+            err.message)
+        : (err as Error)?.message ?? "Ошибка загрузки";
 
-    // категории: ?category=a&category=b
-    if (Array.isArray(category) && category.length > 0) {
-      category.forEach((c) => params.append("category", c));
+      return rejectWithValue(message);
     }
-
-    // сортировка: _sort=price&_order=asc
-    if (sort) params.set("_sort", sort);
-    if (order) params.set("_order", order);
-
-    const qs = params.toString();
-    const url = `/products${qs ? `?${qs}` : ""}`;
-
-    const res = await axiosInstance.get<Product[]>(url);
-    return res.data; // массив товаров
-  } catch (err: unknown) {
-    const message = axios.isAxiosError(err)
-      ? ((err.response?.data as { message?: string } | undefined)?.message ?? err.message)
-      : ((err as Error)?.message ?? "Ошибка загрузки");
-
-    return rejectWithValue(message);
   }
-});
+);
